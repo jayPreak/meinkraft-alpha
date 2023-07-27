b6c5c127bfa638907ce0a9606d55f55f16a24ad9
jayPreak <jayesh.preak2003@gmail.com>
2023-07-27 16:03:44 +0530

    yep

--- src/index.js
+++ src/index.js
@@ -11,46 +11,46 @@ function Overlay() {
       <div className="dot" />
       <div className={`fullscreen bg ${ready ? "ready" : "notready"} ${ready && "clicked"}`}>
         <div className="stack" style={{ fontFamily: "MyFont" }}>
-          <button onClick={() => set(true)}>Start</button>
+          <button onClick={() => set(true)}>PLAY</button>
         </div>
         {/* <Foo wilter date="26. July" year="2023" /> */}
-        <h1 class="absolute top-10 text-9xl font-extrabold text-gray-900 " style={{ fontFamily: "MyFont" }}>
-          <span class="text-transparent bg-clip-text bg-gradient-to-r to-[#a89e9d] from-[#5df54a]">Meinkraft - Alpha</span>
+        <h1 className="absolute top-10 text-8xl font-extrabold text-gray-900 " style={{ fontFamily: "MyFont" }}>
+          <span className="text-transparent bg-clip-text bg-gradient-to-r to-[#a89e9d] from-[#5df54a]">Meinkraft - Alpha</span>
         </h1>
-        <footer class=" rounded-lg shadow  absolute bottom-0 w-full" style={{ fontFamily: "MyFont" }}>
-          <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-            <div class="sm:flex sm:items-center sm:justify-between">
-              <a href="https://jayeshbhushan.me/" class="flex items-center mb-4 sm:mb-0">
-                {/* <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" /> */}
-                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Jayesh Bhushan</span>
+        <footer className=" rounded-lg shadow  absolute bottom-0 w-full" style={{ fontFamily: "MyFont" }}>
+          <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+            <div className="sm:flex sm:items-center sm:justify-between">
+              <a href="https://jayeshbhushan.me/" className="flex items-center mb-4 sm:mb-0">
+                {/* <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" /> */}
+                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Jayesh Bhushan</span>
               </a>
-              <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+              <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                 <li>
-                  <a href="https://github.com/jayPreak" class="mr-4 hover:underline md:mr-6 ">
+                  <a href="https://github.com/jayPreak" className="mr-4 hover:underline md:mr-6 ">
                     Github
                   </a>
                 </li>
                 <li>
-                  <a href="https://github.com/jayPreak" class="mr-4 hover:underline md:mr-6">
+                  <a href="https://github.com/jayPreak" className="mr-4 hover:underline md:mr-6">
                     Twitter
                   </a>
                 </li>
                 <li>
-                  <a href="https://www.linkedin.com/in/jayesh-bhushan-587616200/" class="mr-4 hover:underline md:mr-6 ">
+                  <a href="https://www.linkedin.com/in/jayesh-bhushan-587616200/" className="mr-4 hover:underline md:mr-6 ">
                     LinkedIn
                   </a>
                 </li>
                 <li>
-                  <a href="mailto:jayesh.preak2003@gmail.com" class="hover:underline">
+                  <a href="mailto:jayesh.preak2003@gmail.com" className="hover:underline">
                     Email
                   </a>
                 </li>
               </ul>
             </div>
-            <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-            <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+            <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+            <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
               © 2023{" "}
-              <a href="https://jayeshbhushanme/" class="hover:underline">
+              <a href="https://jayeshbhushanme/" className="hover:underline">
                 JayeshBhushan™
               </a>
               . All Rights Reserved.
